81b29db70630891bb15d272de03d03be4e3b2709
shyamsm5473 <shyamsm5473@gmail.com>
2026-07-28 09:28:45 +0530

    Update settings.py

--- iiitl_project/settings.py
+++ iiitl_project/settings.py
@@ -33,7 +33,7 @@ env = environ.Env(
 environ.Env.read_env(BASE_DIR / '.env')
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = env('SECRET_KEY', default='django-insecure-local-dev-key-change-me-in-prod')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
